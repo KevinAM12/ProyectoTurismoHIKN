c1b9805c76f2ab98344f876db5603b91ae8545b0
KevinAM12 <kalvaradom@vlacademy.com>
2025-06-01 20:59:23 -0600

    Mi primer commit

---
+++ src/app/testimonios/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TestimonioComponent from "../components/TestimonioComponent";
+
+const TestimonioPage : React.FC = () => {
+    
+    return (<div>
+        <h1>Esta es mi pagina de testimonio</h1>
+        <TestimonioComponent></TestimonioComponent>
+        <TestimonioComponent></TestimonioComponent>
+        <TestimonioComponent></TestimonioComponent>
+        <TestimonioComponent></TestimonioComponent>
+    </div>)
+
+}
+
+export default TestimonioPage;
